a8cab81200adfba09a613deb2ce322c7f4dd96ff
Naaav28 <naaav28@gmail.com>
2024-07-25 14:48:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7230057291:AAHxm42L50Cuk4GN5k74Et-a57H_MMCcRs0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Naaav28")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("5588464519")
