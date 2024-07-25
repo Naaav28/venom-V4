0d8db82df0459f1eceb73db0cda1f9a8a2c34ed6
Naaav28 <naaav28@gmail.com>
2024-07-25 15:26:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 BOT_TOKEN=("7230057291:AAHxm42L50Cuk4GN5k74Et-a57H_MMCcRs0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("naaav28")
+OWNER_USERNAME=("Naaav28")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5957809692")
 
 
 
